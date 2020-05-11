16a2287c23bf6e40ead150063b531c6f593a1861
DEEPAK-crypto <deepsdeep45@gmail.com>
2020-05-11 17:30:06 +0530

    Start server

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 var express = require("express");
 var app = express();
+
+// "/" => "This is an Express App"
+app.get("/", function(req, res) {
+    res.send("This is an Express App");
+});
+
+
+// Tell express to listen for requests (start server)
+app.listen(3000, function() {
+    console.log("Sever has started");
+});
